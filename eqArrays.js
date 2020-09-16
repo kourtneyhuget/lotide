96427726483b6f7a1b090bb66683c8a007eceb4f
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-09-16 03:00:22 +0000

    Implementing eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❤️ ❤️ ❤️ Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`😭😭😭Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["Hello"], ["Hello"]), true);
+assertEqual(eqArrays(["No thank you"], ["Yes Please"]), false);
