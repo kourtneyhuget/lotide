8590a405ebf2c42a02e6ed0ca0277e13c1f2914b
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-09-16 23:18:07 +0000

    countLetters function

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❤️ ❤️ ❤️ Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`😭😭😭Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  let results = {};
+  for (letter of string) {
+   if (letter != " ") {
+    if (results[letter]) {
+      results[letter] += 1
+    } else { results[letter] = 1
+    }
+   } 
+  } 
+  return results;
+}
+
+console.log(countLetters("Lighthouse in the house"));
