fe33c354520a54a6a88686443a4c2b57bdbf525d
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-09-16 03:42:48 +0000

    refactored it

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,4 @@
-const assertArraysEqual = function(arr1,arr2){
-  const result = eqArrays(arr1,arr2)
-    if (!result){
-        console.log(`❌ ❌ ❌: OH NO! These arrays are NOT equal`);
-    }
-    else {
-        console.log(`✅ ✅ ✅ : WOOHOO! These arrays ARE equal`);
-    }
-};
+const assertArraysEqual = function(arr1,arr2) {
+  eqArrays(arr1,arr2) ? console.log(`✅ ✅ ✅ : WOOHOO! These arrays ARE equal`) :
+  console.log(`❌ ❌ ❌: OH NO! These arrays are NOT equal`);
+}
