cb2d9cf27bbf05ad1e880b4cfb724f56302fc44b
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-09-16 14:20:14 +0000

    cleaned up the code

--- eqArrays.js
+++ eqArrays.js
@@ -6,19 +6,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+const eqArrays = (arr1, arr2) => {
+  let arrayLengthCheck = arr1.length === arr2.length;
+  if (arrayLengthCheck){
+    for (let i = 0; i < arr1.length; i++){
+      if (arr1 [i] !== arr2 [i]){
+        return false;
+      }
     }
-  }
+  } 
   return true;
 }
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1,2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays(["Hello"], ["Hello"]), true);
 assertEqual(eqArrays(["No thank you"], ["Yes Please"]), false);
