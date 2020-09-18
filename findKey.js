3905b5a202566aa4e1d9b284d97c58f9e454413c
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-09-18 02:38:58 +0000

    find key function

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`❤️ ❤️ ❤️ Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`😭😭😭Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const findKey = function (object, callback) {
+  for (let key in object) { //want to loop through object until we find the right property
+    const wantedKey = callback(object[key]);
+    if (wantedKey) {
+      return key;
+    }
+  }
+  return undefined;
+};
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+console.log(result);
+
+assertEqual(result, "noma");
+assertEqual(result, "elBulli");
